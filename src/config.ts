f8727658513bd3c36281ca5d30e20357fd6f84b8
Aex <spryti@qq.com>
2020-04-30 17:29:49 +0800

    优化部署脚本

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,12 @@
+import { die } from '.'
+
 export default new class {
+
   env = ''
   selfAppId = ''
 
-  set ({ env, selfAppId }: { env: string, selfAppId: string }) {
-    this.env = env
-    this.selfAppId = selfAppId
+  set (config: { env: string, selfAppId: string }) {
+    this.env = config.env || die.hint('缺少env配置')
+    this.selfAppId = config.selfAppId || die.hint('缺少selfAppId配置')
   }
 }
