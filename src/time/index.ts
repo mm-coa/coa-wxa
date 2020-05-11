da5046142a9b2c5f602d8923d94fd69b6035097a
Aex <spryti@qq.com>
2020-05-11 14:10:50 +0800

    优化time插件

--- src/time/index.ts
+++ src/time/index.ts
@@ -1,14 +1,11 @@
-const oneMinute = 60 * 1000
+const oneSecond = 1000
+const oneMinute = 60 * oneSecond
 const oneHour = 60 * oneMinute
 const oneDay = 24 * oneHour
+const oneWeek = 7 * oneDay
+const oneMonth = 30 * oneDay
+const oneYear = 365 * oneDay
+const oneCentury = 100 * oneYear
+const forever = 100 * oneCentury
 
-export default new class {
-  oneHour = oneHour
-  oneDay = oneDay
-
-  onWeek = 7 * oneDay
-  oneMonth = 30 * oneDay
-  oneYear = 365 * oneDay
-
-  forever = 100 * 100 * this.oneYear
-}
+export default { oneSecond, oneMinute, oneHour, oneDay, oneWeek, oneMonth, oneYear, oneCentury, forever }
